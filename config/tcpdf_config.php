185d0a30bf68acdd73d75408dd999c2b1375eb84
Giiiiiiin <miguel_malisa@dlsl.edu.ph>
2024-06-07 18:02:41 +0800

    ...

--- config/tcpdf_config.php
+++ config/tcpdf_config.php
@@ -69,13 +69,12 @@
  * Deafult image logo used be the default Header() method.
  * Please set here your own logo or an empty string to disable it.
  */
-//define ('PDF_HEADER_LOGO', '');
+define ('PDF_HEADER_LOGO', 'image_demo.jpg');
 
 /**
  * Header logo image width in user units.
  */
-//define ('PDF_HEADER_LOGO_WIDTH', 0);
-
+define ('PDF_HEADER_LOGO_WIDTH', 10);
 /**
  * Cache directory for temporary files (full path).
  */
